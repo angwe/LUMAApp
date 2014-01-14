5df56cc66d4ddbdf4afb97ad75b01f437736536e
R Desmond <angwe23@yahoo.com>
2014-01-14 14:04:49 -0600

    Added marker animation script for easier animation. Also, I think the marker clearing should work (it should remove the extraneous markers, reducing the memory load).

--- DeSmetUI.js
+++ DeSmetUI.js
@@ -2,8 +2,6 @@ $(document).ready( function () {
 		
 	$("#layers").append("<p>Text added</p>");
 	
-	var letterObjects = {};
-	var markerObjects = {};
 	
 	var StLouis = new google.maps.LatLng(38.78587,-90.326206);
 	
@@ -23,49 +21,34 @@ $(document).ready( function () {
 		};
 		$('#map-canvas').gmap('addShape', 'Polyline', PathOpts);
 	};
-	
+
 	var pathAnimate = function (marker) {
+		var startPoint;
+		var endPoint;
 		
 		if (marker.letter.origin === '') {
 			startPoint = StLouis;
 		} else {
-			var startPoint = marker.letter.origin.replace(/ /g,'').split(',');
-			startPoint = new google.maps.LatLng(startPoint[0],startPoint[1]);
+			var sp = marker.letter.origin.replace(/ /g,'').split(',');
+			startPoint = new google.maps.LatLng(sp[0],sp[1]);
 		}
 		
-		var endPoint = marker.letter.location.replace(/ /g,'').split(',');
-		endPoint = new google.maps.LatLng(endPoint[0],endPoint[1]);
+		//console.log("startPoint is ", startPoint, " while letter.origin is ", marker.letter.origin, ".");
+		//console.log(startPoint instanceof google.maps.LatLng);
 		
-		var basePath = new google.maps.Polyline( {
-				path: [startPoint,endPoint],
-				visible: false
-		});
-		
-		var length = basePath.Distance();
-		
-		var stepLength = (length / 10);
-		
-		var stepArray = basePath.GetPointsAtDistance(stepLength);
-		
-		var stepNumber = 0;
+		var ep = marker.letter.location.replace(/ /g,'').split(',');
+		endPoint = new google.maps.LatLng(ep[0],ep[1]);
 		
 		var aniMarkerOpts = {
-			position: stepArray[stepNumber],
-      visible: true,
-			icon: './mapbox-maki/src/airport-24.svg'
+			position: startPoint,
+			visible: true,
+			icon: './mapbox-maki/src/post-24.svg',
+			markerType: 'animation',
 		};
 		
 		var aniMarker = $('#map-canvas').gmap('addMarker', aniMarkerOpts);
 		
-		(function myLoop (i) {
-			setTimeout(function () {
-				stepNumber++;
-				aniMarker['0'].set('position',stepArray[stepNumber]);
-				if (--i) myLoop(i);
-			}, 500)
-		})(10);
-		
-		//aniMarker['0'].set('visible','false');
+		aniMarker['0'].animateTo(endPoint, {easing: "easeInOutCubic"});
 		
 	};
 				
@@ -76,11 +59,11 @@ $(document).ready( function () {
 		var markerIndex = 0;
 		var stopLoop = Object.keys(markerObjects).length;
 		var tmpMarker = markerObjects[markerIndex].val(this);
-		pathAnimate(tmpMarker['0']);
+		//pathAnimate(tmpMarker['0']);
 		(function myLoop (i) {          
 				setTimeout(function () {   
 						var marker = markerObjects[markerIndex].val(this);
-						//pathAnimate(marker['0']);
+						pathAnimate(marker['0']);
 						marker['0'].set('visible', true);
 						markerIndex++;
 						if (--i) myLoop(i);      //  decrement i and call myLoop again if i > 0
@@ -104,62 +87,66 @@ $(document).ready( function () {
 	var myMap = $("#map-canvas").gmap('get','map');
 
 	$("#layers").append("<p>Layers on/off will be here</p>");
-		
-	$("#AddMarker").click(function () {
-
-		var fullQuery = encodeURI("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT * FROM 1eja-Sf9fKtfUyWs7gbPVeXC8O45JbIVjWiEtZDE WHERE Location > '' ORDER BY Date&key=AIzaSyCCiTJbrAoe15VipHG5QA2YkMjL3FTgq5Q&typed=true&callback=?");
 	
+		var fullQuery = encodeURI("https://www.googleapis.com/fusiontables/v1/query?sql=SELECT * FROM 1eja-Sf9fKtfUyWs7gbPVeXC8O45JbIVjWiEtZDE WHERE Location > '' ORDER BY Date&key=AIzaSyCCiTJbrAoe15VipHG5QA2YkMjL3FTgq5Q&typed=true&callback=?");
+
+	var letterObjects = {};
+	var markerObjects = {};
+	
+	
+	$.ajax({
+		type: "GET",
+		url: fullQuery,
+		dataType: "jsonp",
+		success: function (jsonResp) {
+			var data = jsonResp['rows'];
+			for (var i in data) {
+				var letter = {
+					date: data[i][0],
+					author: data[i][1],
+					recipient: data[i][2],
+					locationName: data[i][3],
+					information: data[i][4],
+					location: data[i][6],
+					origin: data[i][7]
+				};
+				letterObjects[i] = letter;
+				markerObjects[i] = $('#map-canvas').gmap('addMarker', {
+					'position': letter.location,
+					'bounds':'false',
+					'animation':'none',
+					'optimized':'false',
+					'icon': './mapbox-maki/src/post-24.svg',
+					'visible': false,
+					'letter':letter,
+					'markerType':'letter'
+				});
+			};
+			for (var j in Object.keys(markerObjects)) {
+				markerObjects[j].click(function () {
+					infoWindow(this);
+				});
+				markerObjects[j].dblclick(function () {
+						pathAnimate(this);
+				});
+			};
+		},
+		error: function () { alert("AJAX error!") }
+	});
 
 		
-		$.ajax({
-			type: "GET",
-			url: fullQuery,
-			dataType: "jsonp",
-			success: function (jsonResp) {
-				var data = jsonResp['rows'];
-				for (var i in data) {
-					var letter = {
-						date: data[i][0],
-						author: data[i][1],
-						recipient: data[i][2],
-						locationName: data[i][3],
-						information: data[i][4],
-						location: data[i][6],
-						origin: data[i][7]
-					};
-					letterObjects[i] = letter;
-					markerObjects[i] = $('#map-canvas').gmap('addMarker', {
-						'position': letter.location,
-						'bounds':'false',
-						'animation':'none',
-						'optimized':'false',
-						/*'icon': {
-							path: postPath,
-							fillColor: 'blue',
-							fillOpacity: 0.5
-						},*/
-						'icon': './mapbox-maki/src/post-24.svg',
-						'visible': false,
-						'letter':letter
-					});
-				};
-				for (var j in Object.keys(markerObjects)) {
-					markerObjects[j].click(function () {
-						infoWindow(this);
-					});
-					markerObjects[j].dblclick(function () {
-							pathAnimate(this);
-					});
-				};
-			},
-			error: function () { alert("AJAX error!") }
-		});
+	$("#AddMarkers").click(function () {
 		
 		$('#controls').append("<p>Click an icon to see the info. Double-click to see the delivery path. (Animation shows when click Add Path.)</p>");
+		animateLetters();
+		
+	});
+	
+	$("#ClearAnimations").click(function () {
+			
+			$('#map-canvas').gmap('find', 'markers', { 'property': 'markerType', 'value': 'animation' }, function(marker, found) {
+					marker.setVisible(found);
+			});
 	});
 		
-	$("#AddPath").click(function() {
-			animateLetters();
-	});
-
 });
